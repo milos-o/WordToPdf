9da6e5a547b5969808934fe0399c3096817c1780
Miloš Ostojić <61186768+milos-o@users.noreply.github.com>
2021-01-08 22:17:10 +0100

    changes

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const upload = require("express-fileupload");
-const replaceExt = require('replace-ext');
 const fs = require('fs');
 const path = require('path');
 var docxConverter = require('docx-pdf');
 const hbs = require("hbs");
-const libre = require('libreoffice-convert');
 var nodemailer = require('nodemailer');
-
 
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -19,98 +16,106 @@ app.use(upload())
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + "/public"));
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'milos.osto11@gmail.com',
-      pass: process.env.PASSWORD
-    }
-  });
-  
+const extend_pdf = '.pdf'
+const extend_docx = '.docx'
+var down_name;
+
+ var transporter = nodemailer.createTransport({
+     service: 'gmail',
+     auth: {
+       user: 'milos.osto11@gmail.com',
+       pass: process.env.PASSWORD
+     }
+   });
 
 
 app.get('/', function(req, res) {
   res.render("home.hbs");
 })
 
-let numberOfConverted = 0;
-app.post('/upload', function(req, res) {
-  
+app.post('/upload', function(req, res) {  
   if(req.files.upfile){
-    const file = req.files.upfile;
+    var file = req.files.upfile;
     
-    let name = file.name;
-    let type = file.mimetype;
-    console.log(type)
-    if(type !== 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' && type !== 'application/msword'){
-      res.send("We only accept docx or doc files!");
-    }
-  
-    const uploadpath = __dirname + '/uploads/' + file.name;
-    const filePosiljka = __dirname + '/output.pdf';
-    docxConverter(uploadpath,'./output.pdf',function(err,result){
-      if(err){
-        console.log(err);
-      }
-      console.log('result'+result);
-    });
-  
-    var mailOptions = {
-        from: 'milos.osto11@gmail.com',
-        to: 'milos.gemini@gmail.com',
-        subject: 'Sending Email using Node.js',
-        attachments: [ 
-            {
-                path: 'output.pdf'
-            }
-        ]
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+    name = file.name;
 
-    file.mv(uploadpath, function(err){
+    type = file.mimetype;
+   
+    var uploadpath = __dirname + "/uploads/" + name;
+
+    const First_name = name.split('.')[0];
+
+    down_name = First_name;
+
+    file.mv(uploadpath, function (err){
       if(err){
-        console.log("File Upload Failed",name,err);
-        res.send("Error Occured!")
+
+        console.log(err);
+        console.log("prva");
       }
-      else {
-        numberOfConverted++;
-          //res.setHeader('Content-Length', file.size);
-         // res.setHeader("Content-Type", "application/pdf; charset=utf-8");
-         // res.setHeader("Content-Disposition", `inline; filename=output.pdf`);
-        //  res.send(`<a href=${filePosiljka} download=output.pdf>Download</a>`);
-        res.download(filePosiljka);
-        fs.unlink(uploadpath, (err) => {
-          if (err) {
+      else{
+        var initialPath = path.join(__dirname, `./uploads/${First_name}${extend_docx}`);
+        var uploadPath = path.join(__dirname, `./uploads/${First_name}${extend_pdf}`);
+
+        docxConverter(initialPath, uploadPath, function (err, result){
+          if(err){
             console.log(err);
+            console.log("druga");
           }
-          console.log('FILE [' + file.name + '] REMOVED!');
+          console.log('result'+result);
+          var mailOptions = {
+                 from: 'milos.osto11@gmail.com',
+                 to: req.body.email,
+               subject: 'Sending Email using Node.js',
+                attachments: [
+                     {
+                       path: uploadPath
+                    }
+                 ]
+               };
+            
+               transporter.sendMail(mailOptions, function(error, info){
+                 if (error) {
+                   console.log(error);
+                 } else {
+                   console.log('Email sent: ' + info.response);
+                   transport.close();
+                 }
+               });
+          res.sendFile(__dirname+'/down_html.html')
         });
-        //res.render("home.hbs", {
-         // path: filePosiljka,
-       //   numberOfConverted
-     //   });
       }
     });
-  }
-  else {
+
+  }else{
     res.send("No File selected !");
     res.end();
-  };
+  }
 })
 
+app.get('/download', (req,res) =>{
+  //This will be used to download the converted file
+  res.download(__dirname +`/uploads/${down_name}${extend_pdf}`,`${down_name}${extend_pdf}`,(err) =>{
+    if(err){
+      res.send(err);
+    }else{
+      //Delete the files from uploads directory after the use
+      console.log('Files deleted');
+      const delete_path_doc = process.cwd() + `/uploads/${down_name}${extend_docx}`;
+      const delete_path_pdf = process.cwd() + `/uploads/${down_name}${extend_pdf}`;
+      try {
+        fs.unlinkSync(delete_path_doc)
+        fs.unlinkSync(delete_path_pdf)
+        //file removed
+      } catch(err) {
+        console.error(err)
+      }
+    }
+  })
+})
 
-app.listen(3000); 
+app.get('/thankyou',(req,res) => {
+  res.sendFile(__dirname+'/thankyou.html')
+})
 
-/*  fs.unlink(filePosiljka, (err) => {
-          if (err) {
-            console.log(err);
-          }
-          console.log('FILE [' + file.name + '] REMOVED!');
-        });*/
+app.listen(3000);
